246b255107fbcef944a6fce336b81aff6e1532b5
chheangg <balldremz@gmail.com>
2023-03-07 15:30:26 +0700

    Implement Task Updating functionalities

--- lib/components/task_tile.dart
+++ lib/components/task_tile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:todo_app/screens/form_screen.dart';
 import '../models/models.dart';
 
 class TaskTile extends StatefulWidget {
@@ -17,70 +18,88 @@ class _TaskTileState extends State<TaskTile> {
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
-    return Container(
+    return Ink(
       decoration: BoxDecoration(
-        color: Colors.grey[200],
+        color: Colors.grey[300],
         borderRadius: BorderRadius.circular(8.0),
       ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Container(
+          Ink(
             height: 75,
             width: width - 25,
             decoration: BoxDecoration(
                 color: Colors.grey[300],
                 borderRadius: const BorderRadius.all(Radius.circular(8.0))),
-            child: Row(children: [
-              Container(
-                height: 75,
-                width: 8,
-                decoration: BoxDecoration(
-                    color: widget.task.color,
-                    borderRadius: const BorderRadius.only(
-                        topLeft: Radius.circular(8.0),
-                        bottomLeft: Radius.circular(8.0))),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(right: 4.0),
-                child: ElevatedButton(
-                  onPressed: () {
-                    final changeStatus =
-                        Provider.of<TaskManager>(context, listen: false)
-                            .changeStatusTask;
-                    changeStatus(widget.task);
-                  },
-                  style: ButtonStyle(
-                      shape: MaterialStateProperty.all<CircleBorder>(
-                        CircleBorder(
-                          side: BorderSide(
-                            color: widget.task.color,
-                            width: 2.0,
+            child: InkWell(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (_) => FormScreen(
+                            task: widget.task,
+                            onCreate: (_) {},
+                            onUpdate: (task) {
+                              final updateTask = Provider
+                                .of<TaskManager>(context, listen: false)
+                                .updateTask;
+                              updateTask(task);
+                              Navigator.of(context).pop();
+                            },
+                            isUpdating: true)));
+              },
+              child: Row(children: [
+                Container(
+                  height: 75,
+                  width: 8,
+                  decoration: BoxDecoration(
+                      color: widget.task.color,
+                      borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(8.0),
+                          bottomLeft: Radius.circular(8.0))),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 4.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      final changeStatus =
+                          Provider.of<TaskManager>(context, listen: false)
+                              .changeStatusTask;
+                      changeStatus(widget.task);
+                    },
+                    style: ButtonStyle(
+                        shape: MaterialStateProperty.all<CircleBorder>(
+                          CircleBorder(
+                            side: BorderSide(
+                              color: widget.task.color,
+                              width: 2.0,
+                            ),
                           ),
                         ),
-                      ),
-                      iconColor: MaterialStateProperty.all<Color>(Colors.red),
-                      backgroundColor:
-                          MaterialStateProperty.all<Color>(Colors.white)),
-                  child: widget.task.isDone
-                      ? Icon(Icons.circle, color: widget.task.color, size: 42)
-                      : const Icon(null, size: 42),
+                        iconColor: MaterialStateProperty.all<Color>(Colors.red),
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.white)),
+                    child: widget.task.isDone
+                        ? Icon(Icons.circle, color: widget.task.color, size: 42)
+                        : const Icon(null, size: 42),
+                  ),
                 ),
-              ),
-              _buildMainTaskButton(widget.task.name, widget.task.description),
-              Padding(
-                  padding: const EdgeInsets.only(right: 8.0),
-                  child: IconButton(
-                    onPressed: () {
-                      setState(() {
-                        isExpanded = !isExpanded;
-                      });
-                    },
-                    icon: isExpanded
-                        ? const Icon(Icons.expand_less)
-                        : const Icon(Icons.expand_more),
-                  )),
-            ]),
+                _buildMainTaskButton(widget.task.name, widget.task.description),
+                Padding(
+                    padding: const EdgeInsets.only(right: 8.0),
+                    child: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          isExpanded = !isExpanded;
+                        });
+                      },
+                      icon: isExpanded
+                          ? const Icon(Icons.expand_less)
+                          : const Icon(Icons.expand_more),
+                    )),
+              ]),
+            ),
           ),
           if (isExpanded) _buildExpandedContainer(widget.task.content)
         ],
